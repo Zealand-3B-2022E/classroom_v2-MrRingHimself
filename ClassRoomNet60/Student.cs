3b1a09cfa68db4ef59c6bfdd1a93069a828d8964
Patrick Ellesgaard Ring <patr4174@edu.easj.dk>
2022-09-05 13:19:55 +0200

    Task 1 færdig

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int BirthMonth { get; set; }
+        public int BirthDay { get; set; }
+
+        public Student(string name, int birthMonth, int birthDay)
+        {
+            Name = name;
+            BirthMonth = birthMonth;
+            BirthDay = birthDay;
+        }
+    }
+}
